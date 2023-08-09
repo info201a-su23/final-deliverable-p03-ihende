b3c30e3ff89c3a7e240258f9fd08f1223a931969
ihende <ihende@uw.edu>
2023-08-08 19:15:03 -0700

    adding and setting up tabs

--- ui.R
+++ ui.R
@@ -1,34 +1,43 @@
 
-maternity_leave_by_plot <- tabPanel("Maternity Leave By Country",
-                                    h1("Maternity Leave By Country"),
-                                    
-                                    sidebarLayout(
-                                      sidebarPanel(
-                                        h2("Options for Graph"),
-                                        selectInput(inputId = "country_select",
-                                                    label = "Select Countries",
-                                                    choices = maternity_df$Country.Name,
-                                                    selected = "Ireland",
-                                                    multiple = TRUE)
-                                      ),
-                                      mainPanel(
-                                        h2("Maternity Leave by Country Plot"),
-                                        plotlyOutput(outputId = "maternity_plotly")
-                                      )
-                                    )
+introduction_tab_panel <- tabPanel("Introduction",
+                                    h1("TO DO: Add introduction here.")
 )
 
 
-second_tab_panel <- tabPanel("Second graph",
+graph1_tab_panel <- tabPanel("First graph",
+                             h1("Maternity Leave By Country"),
+                             
+                             sidebarLayout(
+                               sidebarPanel(
+                                 h2("Options for Graph"),
+                                 selectInput(inputId = "country_select",
+                                             label = "Select Countries",
+                                             choices = maternity_df$Country.Name,
+                                             selected = "Bulgaria",
+                                             multiple = TRUE)
+                               ),
+                               mainPanel(
+                                 h2("Maternity Leave by Country Plot"),
+                                 plotlyOutput(outputId = "maternity_plotly")
+                               )
+                             ))
+
+graph2_tab_panel <- tabPanel("Second graph",
                              h1("TO DO: Add another graph here.")
 )
 
-third_tab_panel <- tabPanel("Third graph",
-                             h1("TO DO: Add another graph here.")
+graph3_tab_panel <- tabPanel("Third graph",
+                            h1("TO DO: Add another graph here.")
+)
+
+conclusion_tab_panel <- tabPanel("Conclusion/Summary Takeaways",
+                            h1("TO DO: Add conclusion here.")
 )
 
 ui <- navbarPage("Maternity Leave",
-                 maternity_leave_by_plot,
-                 second_tab_panel,
-                 third_tab_panel
+                 introduction_tab_panel,
+                 graph1_tab_panel,
+                 graph2_tab_panel,
+                 graph3_tab_panel,
+                 conclusion_tab_panel
 )
